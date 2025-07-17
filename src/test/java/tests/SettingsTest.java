ec9cdd559026fe9a203ea889997e9febdc6bb556
arkaminski <arturkaminski0612@gmail.com>
2025-07-17 18:53:43 +0200

    Tests and pages were added third

---
+++ src/test/java/tests/SettingsTest.java
@@ -0,0 +1,72 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class SettingsTest extends BaseTest {
+    String textLanguage = "Seu idioma foi alterado com sucesso";
+    String textTimeAndAlies = "Your settings have been saved successfully";
+
+    @Test(description = "Language selection settings")
+    public void languageSelectionSettings() throws InterruptedException {
+        loginPage.open()
+                .enterLoginAndPassword(email, password)
+                .userClickButton();
+        settingsPage.clickSettingButton()
+                .selectSettingLanguage();
+        settingsPage.selectLanguage()
+                .clickButtonOkLanguage();
+
+        assertEquals(settingsPage.textMessageLanguage("Seu idioma foi alterado com sucesso"),textLanguage,"Text does not match");
+
+    }
+
+    @Test(description = "logout")
+    public void logout () {
+        loginPage.open()
+                .enterLoginAndPassword(email, password)
+                .userClickButton();
+        settingsPage.logout();
+
+        assertTrue(homePage.isPageOpen());
+    }
+
+    @Test(description = "Adding a login alias instead of email")
+    public void addingAlies() throws InterruptedException, IOException {
+        loginPage.open()
+                .enterLoginAndPassword(email, password)
+                .userClickButton();
+        settingsPage.clickSettingButton()
+                .selectSettingAlies()
+                .nicknameClickCheckBox();
+        settingsPage.selectNickName("Hotel2");
+        settingsPage.clickButtonOkTime();
+
+        assertEquals(settingsPage.getMessageTextAliasAndTime("Your settings have been saved successfully"),textTimeAndAlies,"Text does not match");
+
+        settingsPage.deleteNickName();
+        settingsPage.nicknameClickCheckBox();
+        settingsPage.clickButtonOkTime();
+
+    }
+
+    @Test(description = "Change page time")
+    public void changePageTime() throws InterruptedException {
+        loginPage.open()
+                .enterLoginAndPassword(email, password)
+                .userClickButton();
+        settingsPage.clickSettingButton()
+                .selectSettingTimeout();
+        settingsPage.clickButtonTime()
+                .listTimeLimit()
+                .clickButtonOkTime();
+
+        assertEquals(settingsPage.getMessageTextAliasAndTime("Your settings have been saved successfully"),textTimeAndAlies,"Text does not match");
+
+    }
+
+}
